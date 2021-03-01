bf6b53473f686abb6bced3130490e2c13d6c82c6
Simon Garnotel <simon.garnotel@gmail.com>
2021-03-01 14:06:40 +0100

    cron jobs

---
+++ scripts/cron_backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR \
+    && cd .. \
+    && sh tanatloc.sh db backup \
+    && sh tanatloc.sh data backup

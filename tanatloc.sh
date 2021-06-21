5d955a7ed9dcb8ce48b7fad614590b8cdcedee3d
Simon Garnotel <simon.garnotel@gmail.com>
2021-06-21 14:24:26 +0200

    inverted commands

--- tanatloc.sh
+++ tanatloc.sh
@@ -32,7 +32,7 @@ else
             docker-compose run -e PGPASSWORD="password" database pg_dump -h database -U postgres tanatloc2 > ${dbBackup}/dump-$(date +%Y-%m-%d).sql
         elif [ "$option" = "run" ]
         then
-            docker run -it -v tanatloc-ssr-deploy_tanatlocData:/data ubuntu /bin/bash
+            docker-compose run database psql -U postgres -h database
         else
             echo "Unknown value ${value}. Please see tanaloc.sh --help."
         fi
@@ -43,7 +43,7 @@ else
             docker run -v tanatloc-ssr-deploy_tanatlocData:/data -v $dataBackup:/backup ubuntu tar cvfP /backup/backup-$(date +%Y-%m-%d).tar /data
         elif [ "$option" = "run" ]
         then
-            docker-compose run database psql -U postgres -h database
+            docker run -it -v tanatloc-ssr-deploy_tanatlocData:/data ubuntu /bin/bash
         else
             echo "Unknown value ${value}. Please see tanaloc.sh --help."
         fi

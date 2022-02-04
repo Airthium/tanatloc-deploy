47ede106047ee4f4c675f18c6d08daf7781fb546
Simon Garnotel <simon.garnotel@gmail.com>
2022-02-04 12:11:47 +0100

    fix

--- scripts/ipv6.sh
+++ scripts/ipv6.sh
@@ -5,18 +5,18 @@ status=$1
 if [ "$status" = "OFF" ]
 then
     # Comment ipv6 lines
-    if [ -f ../docker/run.nginx.conf ]
+    if [ -f docker/run.nginx.conf ]
     then
-        sed "s/ listen \[::\]:/ #listen \[::\]:/" docker/nginx.conf > docker/run.nginx.conf
-    else
         sed -i "s/ listen \[::\]:/ #listen \[::\]:/" docker/run.nginx.conf
+    else
+        sed "s/ listen \[::\]:/ #listen \[::\]:/" docker/nginx.conf > docker/run.nginx.conf
     fi
 else
     # Uncomment ipv6 lines
-    if [ -f ../docker/run.nginx.conf ]
+    if [ -f docker/run.nginx.conf ]
     then
-        sed "s/#listen \[::\]:/listen \[::\]:/" docker/nginx.conf > docker/run.nginx.conf
-    else
         sed -i "s/#listen \[::\]:/listen \[::\]:/" docker/run.nginx.conf
+    else
+        sed "s/#listen \[::\]:/listen \[::\]:/" docker/nginx.conf > docker/run.nginx.conf
     fi
 fi

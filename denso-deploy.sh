2976b0905007b9b2b387da81cc41684294590083
Simon Garnotel <simon.garnotel@gmail.com>
2022-02-04 12:30:10 +0100

    denso

--- denso-deploy.sh
+++ denso-deploy.sh
@@ -3,6 +3,10 @@
 echo " - Modify postgres version (old kernel)"
 sed -i 's/postgres:14/postgres:13-buster/g' ./docker-compose.yml
 
+echo " - Clean additional volumes"
+echo "services:" > ./docker-compose.volumes.yml
+echo "  tanatloc:" >> ./docker-compose.volumes.yml
+
 echo " - Set denso tag"
 bash tanatloc.sh set tanatloc_tag denso
 
@@ -15,7 +19,7 @@ bash tanatloc.sh set http_proxy http://in-proxy-o.denso.co.jp:8080
 echo " - Set HTTPS_PROXY"
 bash tanatloc.sh set https_proxy http://in-proxy-o.denso.co.jp:8080
 
-echo "- Set HTTP port"
+echo " - Set HTTP port"
 bash tanatloc.sh set http_port 6543
 
 echo " - Disable IPv6"
